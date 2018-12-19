493566a3f27ad6c09df40a0da0729c2d488d6e2c
Younes Diouri <younesdiouri@gmail.com>
2018-12-19 01:02:38 +0100

    jwt update

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
